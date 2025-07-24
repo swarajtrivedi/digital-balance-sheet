81f93c0107452bbc509a1c454722ff23fcd859de
Swaraj Trivedi <swarajtrivedi1999@gmail.com>
2025-07-24 02:29:20 -0400

    pushing yaml file for automating workflow with EC2

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,6 +5,8 @@ const authRouter  = express.Router();
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
+
+// Test endpoint
 authRouter.post("/test", async(req, res)=>{
     console.log("test working");
     res.status(201);
