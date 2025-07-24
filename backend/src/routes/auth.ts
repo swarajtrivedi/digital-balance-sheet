e4feca06650024610e1d6a6661b148449d03faf0
Swaraj Trivedi <swarajtrivedi1999@gmail.com>
2025-07-24 00:47:21 -0400

    commiting pm2 changes to keep the server running

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,5 +5,9 @@ const authRouter  = express.Router();
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
+authRouter.post("/test", async(req, res)=>{
+    console.log("test working");
+    res.status(201);
+})
 
 export default authRouter;
