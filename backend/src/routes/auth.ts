3dcaf6bea53521945d90bb773ec2e46dafbdd734
Swaraj Trivedi <swarajtrivedi1999@gmail.com>
2025-08-27 12:49:26 -0700

    checking automation yaml

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -9,7 +9,7 @@ authRouter.post("/logout", logoutUser);
 
 // Test endpoint
 authRouter.post("/test", async(req, res)=>{
-    console.log("Checking automation with Elastic IP.");
+    console.log("Checking automation with Elastic IP -1.");
     res.status(201);
 })
 
